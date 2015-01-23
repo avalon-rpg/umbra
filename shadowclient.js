0dde06131582a83cb4514d8c221c62a366cecb24
Kevin Wright <kev.lee.wright@gmail.com>
2015-01-23 13:20:50 +0000

    more logging

--- shadowclient.js
+++ shadowclient.js
@@ -97,7 +97,7 @@ ShadowClient.prototype.init = function(user, pass) {
           console.log('Unexpected login response: ' + line);
         }
       } else {
-        //skip line, not logging in
+        console.log('skipping line before login: ' + line);
       }
 
       return;
