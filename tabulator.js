68b579ea83f3037752118784a40be3fd7840d114
Kevin Wright <kev.lee.wright@gmail.com>
2015-02-06 10:34:10 +0000

    removed unused code and spammy logging

--- tabulator.js
+++ tabulator.js
@@ -11,39 +11,12 @@ function Tabulator() {
 
 var sphereSenseRegex = /(\S+) at ([^\.]+)\.(?:\s+)H: ((?:\d+)\/(?:\d+))(?:\s*)M: ((?:\d+)\/(?:\d+))/;
 
-function parseSphereSense(block) {
-  var rows = [{header: true, cells: ['who', 'where', 'health', 'mana']}];
-  var len = block.entries.length();
-  for(i=1; i < len; ++i) {
-    var entry = block.entries[i];
-    var match = sphereSenseRegex.exec(entry.line);
-    if(match) {
-      rows.push({
-        header: false,
-        cells: [match[1],match[2],match[3],match[4]]
-      });
-    } else {
-      console.log('mismatch: ' + entry.line);
-    }
-  }
-
-  return {
-    monospaced: false,
-    entries: [{
-      qual: 'table',
-      rows: rows
-    }],
-    prompt: block.prompt
-  };
-}
 
 Tabulator.prototype.process = function (block) {
   if(!block.entries || !block.monospaced) { return block; }
 
   var outEntries = [];
   var rows = null;
-
-  console.log(JSON.stringify(block));
 
   var len = block.entries.length;
   for(i=1; i < len; ++i) {
