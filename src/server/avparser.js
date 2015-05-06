4a5c466095bf370577c2d66128ba44173ba8bde6
Kevin Wright <kev.lee.wright@gmail.com>
2015-05-06 13:42:03 +0100

    factored replaceables out of avparser

--- src/server/avparser.js
+++ src/server/avparser.js
@@ -3,6 +3,7 @@ var util = require('util');
 var stripAnsi = require('strip-ansi');
 var EventEmitter = require('events').EventEmitter;
 var blocks = require('./blocks');
+var replacables = require('./replaceables');
 
 /////////////////////////////////////////
 // Helpful helpers
@@ -271,11 +272,13 @@ AvParser.prototype.init = function(shadowclient) {
         let newBlock = new blocks.Block('avmsg');
         let parts = match[1].split('###');
         let partcount = parts.length;
+        let cmd = '';
         for (var i = 1; i < partcount; i++) {
           var part = parts[i];
           var keyarr = part.split('=', 1);
           var key = keyarr[0];
           var value = part.substring(key.length + 1);
+          if(key === 'cmd') { cmd = value; }
           if(key === 'tag') {
             var tags = value.split(' ');
             tags.push('block');
@@ -285,6 +288,9 @@ AvParser.prototype.init = function(shadowclient) {
           }
         }
         blockStack.push(newBlock);
+        if(cmd.toUpperCase() === 'WHO') {
+          appendLine('You can see the following people in the land:');
+        }
       }
     },{
       regex: /^###end@.*$/,
@@ -468,72 +474,6 @@ AvParser.prototype.init = function(shadowclient) {
           content:  match[2]
         });
       }
-    },{
-      regex: /^(\S+) has just departed beyond the confines of your sphere of control\.$/,
-      func: function(match, rawLine) {
-        var who = match[1].toLowerCase().replace('(','').replace(')','');
-        appendReplacableLine(rawLine, 'oracle-sphere-movement-' + who);
-      }
-    },{
-      regex: /^(\S+) has just stepped within your sphere of control\.$/,
-      func: function(match, rawLine) {
-        var who = match[1].toLowerCase().replace('(','').replace(')','');
-        appendReplacableLine(rawLine, 'oracle-sphere-movement-' + who);
-      }
-    },{
-      regex: /^The ethereal flow continues to be absorbed into your seedpod: (\d+) seeds absorbed into (\d+) potency within the pod\.$/,
-      func: function(match, rawLine) {
-        appendReplacableLine(rawLine, 'oracle-pod-absorb-self');
-      }
-    },{
-      regex: /^(?:You note marks of your own ethereal seed below\. )?Ethereal marking at "(.*)" now at: (\d+)\.$/,
-      func: function(match, rawLine) {
-        let loc = match[1].replace(/\s/g, '-').replace(/'/g, "").replace(/"/g, '');
-        appendReplacableLine(rawLine, 'oracle-marking-' + loc);
-      }
-    },{
-      regex: /^(?:You note marks of your own ethereal seed below\. )?You allow an ethereal seed of ether to fall through the oracle-eye. The seed markings at "(.+)" now number (.+)\.$/,
-      func: function(match, rawLine) {
-        let loc = match[1].replace(/\s/g, '-').replace(/'/g, "").replace(/"/g, '');
-        appendReplacableLine(rawLine, 'oracle-marking-' + loc);
-      }
-    },{
-      regex: /^Below at "(.+)" the ether continue to twine and thicken about the (.+), the wall (.*) percent towards completion\.$/,
-      func: function(match, rawLine) {
-        let dirn = match[2];
-        appendReplacableLine(rawLine, 'oracle-wall-building-' + dirn);
-      }
-    },{
-      regex: /^The oracle-eye focus is unusually sharp as it draws up a sparkling flow of ether from the (.+) of "(.+)", directing it (.*) to gather density; (.*) percent complete\.$/,
-      func: function(match, rawLine) {
-        let srcdirn = match[1];
-        let tgtdirn = match[3];
-        appendReplacableLine(rawLine, 'oracle-raising-' + tgtdirn);
-      }
-    },{
-      regex: /^(.+) is fully manifested at the (.+) reaches of the wind; the last of the sparkling flow having passed up through the oracle-eye.$/,
-      func: function(match, rawLine) {
-        let tgtdirn = match[2];
-        appendReplacableLine(rawLine, 'oracle-raising-' + tgtdirn);
-      }
-    },{
-      regex: /^Ether continues to flow through the eye, from the (.+) region to "(.+)" below. The supercharged particles gather at the (.+), enlarging (\d+)% of the ethereal creation\.$/,
-      func: function(match, rawLine) {
-        let dirn1 = match[1];
-        let dirn2 = match[3];
-        appendReplacableLine(rawLine, 'oracle-lowering-' + dirn1 + '-' + dirn2);
-      }
-    },{
-      regex: /^(.+) has dropped seed markings below\.$/,
-      func: function(match, rawLine) {
-        appendReplacableLine(rawLine, 'oracle-othermarks-' + match[1]);
-      }
-    },{
-      regex: /^The battle for supremacy with (.*) over "(.*)" continues: (.*) further focus bouts and the attack (.*) will be completed, permanently expanding the (.*) sphere of control\.$/,
-      func: function(match, rawLine) {
-        let dirn = match[4];
-        appendReplacableLine(rawLine, 'far-attack-' + dirn);
-      }
     }
   ];
 
@@ -545,6 +485,8 @@ AvParser.prototype.init = function(shadowclient) {
 
   var onLine = function (line) {
 
+    let cleanLine = stripAnsi(line);
+
     let seqLen = sequences.length;
     for (let i = 0; i < seqLen; i++) {
       let entry = sequences[i];
@@ -553,7 +495,6 @@ AvParser.prototype.init = function(shadowclient) {
         if(entry.ansiRegex) {
           match = entry.ansiRegex.exec(line);
         } else if(entry.regex) {
-          let cleanLine = stripAnsi(line);
           match = entry.regex.exec(cleanLine);
         } else {
           console.error("Parser entry with no regex defined: " + JSON.stringify(entry));
@@ -565,8 +506,13 @@ AvParser.prototype.init = function(shadowclient) {
       }
     }
 
-    //default fallback
-    appendLine(line);
+    let tag = replacables.attempt(cleanLine);
+    if(tag) {
+      appendReplacableLine(line, tag);
+    } else {
+      //default fallback
+      appendLine(line);
+    }
   };
 
 
---
+++ src/server/replaceables.js
@@ -0,0 +1,58 @@
+'use strict';
+
+function Replaceables() {
+  function escape(text) {
+    return text.toLowerCase()
+      .replace(/\(/g,'')
+      .replace(/\)/g,'')
+      .replace(/\s+/g, '-')
+      .replace(/'/g, '')
+      .replace(/"/g, '');
+  }
+
+  const entries = [
+    {
+      regex: /^(\S+) has just departed beyond the confines of your sphere of control\.$/,
+      func: function(match) { return 'oracle-sphere-movement-' + escape(match[1]); }
+    },{
+      regex: /^(\S+) has just stepped within your sphere of control\.$/,
+      func: function(match) { return 'oracle-sphere-movement-' + escape(match[1]); }
+    },{
+      regex: /^The ethereal flow continues to be absorbed into your seedpod: (\d+) seeds absorbed into (\d+) potency within the pod\.$/,
+      func: function() { return 'oracle-pod-absorb-self'; }
+    },{
+      regex: /^(?:You note marks of your own ethereal seed below\. )?Ethereal marking at "(.*)" now at: (\d+)\.$/,
+      func: function(match) { return 'oracle-marking-' + escape(match[1]); }
+    },{
+      regex: /^(?:You note marks of your own ethereal seed below\. )?You allow an ethereal seed of ether to fall through the oracle-eye. The seed markings at "(.+)" now number (.+)\.$/,
+      func: function(match) { return 'oracle-marking-' + escape(match[1]); }
+    },{
+      regex: /^Below at "(.+)" the ether continue to twine and thicken about the (.+), the wall (.*) percent towards completion\.$/,
+      func: function(match) { return 'oracle-wall-building-' + match[2]; }
+    },{
+      regex: /^The oracle-eye focus is unusually sharp as it draws up a sparkling flow of ether from the (.+) of "(.+)", directing it (.*) to gather density; (.*) percent complete\.$/,
+      func: function(match) { return 'oracle-raising-' + match[3]; }
+    },{
+      regex: /^(.+) is fully manifested at the (.+) reaches of the wind; the last of the sparkling flow having passed up through the oracle-eye.$/,
+      func: function(match) { return 'oracle-raising-' + match[2]; }
+    },{
+      regex: /^Ether continues to flow through the eye, from the (.+) region to "(.+)" below. The supercharged particles gather at the (.+), enlarging (\d+)% of the ethereal creation\.$/,
+      func: function(match) { return 'oracle-lowering-' + match[1] + '-' + match[3]; }
+    },{
+      regex: /^(.+) has dropped seed markings below\.$/,
+      func: function(match) { return 'oracle-othermarks-' + match[1]; }
+    },{
+      regex: /^The battle for supremacy with (.*) over "(.*)" continues: (.*) further focus bouts and the attack (.*) will be completed, permanently expanding the (.*) sphere of control\.$/,
+      func: function(match) { return 'far-attack-' + match[4]; }
+    }
+  ];
+
+  this.attempt = function(text) {
+    entries.forEach(function(entry) {
+      let match = entry.regex.exec(text);
+      if(match) { return entry.func(match); }
+    });
+  };
+}
+
+module.exports = new Replaceables();
